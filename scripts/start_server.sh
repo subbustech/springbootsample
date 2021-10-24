bb7003066f00a3f9c726b2cb62b5b0525123d380
subbu <padalasubbu80@gmail.com>
2021-10-24 21:07:23 +0530

    Shell scripts updated.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 
 #!/bin/bash
-service tomcat8 start
+$BASE/startup.sh
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 isExistApp=`pgrep java`
 if [[ -n  $isExistApp ]]; then
-   service tomcat8 stop
+   $BASE/shutdown.sh
 fi
