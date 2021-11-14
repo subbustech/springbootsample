73a11f5b669bf55f2c7d2af513827c65bd21cc8e
subbu <padalasubbu80@gmail.com>
2021-11-14 19:07:54 +0530

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-#sudo rm -rf /apache-tomcat-8.5.72/webapps/cpproject*
+cd ../..
+sudo rm -rf /apache-tomcat-8.5.72/webapps/cpproject*
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,6 @@
 
 #!/bin/bash
+cd ../..
 isExistApp=`pgrep java`
 if [[ -n  $isExistApp ]]; then
    sudo /apache-tomcat-8.5.72/bin/shutdown.sh
