7f8cab860e34f28f09cbaf393c0a711d2e806ec5
subbu <padalasubbu80@gmail.com>
2021-11-14 14:25:30 +0530

    test4

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 
 #!/bin/bash
-sudo apache-tomcat-8.5.72/bin/startup.sh
+sudo /apache-tomcat-8.5.72/bin/startup.sh
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 isExistApp=`pgrep java`
 if [[ -n  $isExistApp ]]; then
-   sudo apache-tomcat-8.5.72/bin/shutdown.sh
+   sudo /apache-tomcat-8.5.72/bin/shutdown.sh
 fi
